b608fe6adf56c241ed7c0f5c20da18ff54cb5aa8
Thiago Pereira <pereira.tp.thiago@gmail.com>
2021-04-10 16:53:49 -0300

    Update map.js

--- 2 ano/empreendedorismo/ProjetoPadaria/assets/js/map.js
+++ 2 ano/empreendedorismo/ProjetoPadaria/assets/js/map.js
@@ -3,3 +3,4 @@ const map = L.map("mapid").setView([-25.091355121868734, -50.16051757407875], 15
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 var marker =  L.marker([-25.091355121868734, -50.16051757407875]).addTo(map);
+marker.bindPopup("<a target='_blank' href='https://www.google.com.br/maps/@-25.0912212,-50.1606227,19.5z'>Clica aqui<a>").openPopup();
