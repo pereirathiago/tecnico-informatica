c1b663163c66843c80f68f2ac24e5621a4c5e637
Thiago Pereira <pereira.tp.thiago@gmail.com>
2021-04-11 15:28:31 -0300

    Update index.php

--- 2 ano/empreendedorismo/ProjetoPadaria/assets/php/index.php
+++ 2 ano/empreendedorismo/ProjetoPadaria/assets/php/index.php
@@ -1,17 +1,17 @@
 <?php
     include_once('connect.php');
 
-    if(isset($_POST['nome'])){
-        $nome = mysqli_real_escape_string($connect,$_POST['nome']);
+    if(isset($_POST['name'])){
+        $nome = mysqli_real_escape_string($connect,$_POST['name']);
     }
     if(isset($_POST['email'])){
         $email = mysqli_real_escape_string($connect,$_POST['email']);
     }
-    if(isset($_POST['telefone'])){
-        $telefone = mysqli_real_escape_string($connect,$_POST['telefone']);
+    if(isset($_POST['tel'])){
+        $telefone = mysqli_real_escape_string($connect,$_POST['tel']);
     }
-    if(isset($_POST['msg'])){
-        $msg = mysqli_real_escape_string($connect,$_POST['msg']);
+    if(isset($_POST['message'])){
+        $msg = mysqli_real_escape_string($connect,$_POST['message']);
     }
     if($nome && $email && $telefone && $msg){
         mysqli_query($connect,"INSERT INTO `padaria` (nome,email,telefone,msg) VALUES('$nome','$email','$telefone','$mensagem')");
