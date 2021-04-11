dfddc279a0108e37d98df3b5ac10b97e5607e2a9
Thiago Pereira <pereira.tp.thiago@gmail.com>
2021-04-11 15:26:37 -0300

    index.php

--- 2 ano/empreendedorismo/ProjetoPadaria/assets/php/connect.php
+++ 2 ano/empreendedorismo/ProjetoPadaria/assets/php/connect.php
@@ -1,6 +1,6 @@
 <?php
     try{
-        $connect = mysqli_connect("localhost", "root", "vertrigo", "padria");
+        $connect = mysqli_connect("localhost", "root", "vertrigo", "padaria");
         echo"<script>console.log('Conectou')</script>";
     } catch(Exception $e){
         echo"<script>console.log('Não conectou, erro $e')</script>";
---
+++ 2 ano/empreendedorismo/ProjetoPadaria/assets/php/index.php
@@ -0,0 +1,21 @@
+<?php
+    include_once('connect.php');
+
+    if(isset($_POST['nome'])){
+        $nome = mysqli_real_escape_string($connect,$_POST['nome']);
+    }
+    if(isset($_POST['email'])){
+        $email = mysqli_real_escape_string($connect,$_POST['email']);
+    }
+    if(isset($_POST['telefone'])){
+        $telefone = mysqli_real_escape_string($connect,$_POST['telefone']);
+    }
+    if(isset($_POST['msg'])){
+        $msg = mysqli_real_escape_string($connect,$_POST['msg']);
+    }
+    if($nome && $email && $telefone && $msg){
+        mysqli_query($connect,"INSERT INTO `padaria` (nome,email,telefone,msg) VALUES('$nome','$email','$telefone','$mensagem')");
+    }
+    header('Location: ../index.html');
+    break;
+?>
