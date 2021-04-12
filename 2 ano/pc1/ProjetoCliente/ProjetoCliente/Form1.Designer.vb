dcf7c83ed75b5a34231c7b5396699a607378271d
Thiago Pereira <pereira.tp.thiago@gmail.com>
2021-04-12 14:18:29 -0300

    Update Form1.Designer.vb

--- 2 ano/pc1/ProjetoCliente/ProjetoCliente/Form1.Designer.vb
+++ 2 ano/pc1/ProjetoCliente/ProjetoCliente/Form1.Designer.vb
@@ -203,7 +203,7 @@ Partial Class Form1
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Cliente"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
