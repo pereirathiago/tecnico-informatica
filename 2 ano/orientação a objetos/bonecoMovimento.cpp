6eb56ccabf793a14ba2034a9525da01ebec64900
Thiago Pereira <pereira.tp.thiago@gmail.com>
2021-04-13 09:40:28 -0300

    Update bonecoMovimento.cpp

--- 2 ano/orientação a objetos/bonecoMovimento.cpp
+++ 2 ano/orientação a objetos/bonecoMovimento.cpp
@@ -1,34 +1,27 @@
 #include <iostream>
-
 using namespace std;
-
 class Boneco
 {
-public:
+private:
     int idBoneco;
-    int coordenadaX;
-    int coordenadaY;
 
 public:
-    void getCoordenadas(void)
+    int coordenadaX = 0;
+    int coordenadaY = 0;
+    int movimentacao(int coordenadaX, int coordenadaY)
     {
-        cout << "Digite o valor de X: ";
-        cin >> coordenadaX;
-        cout << "Digite o valor de Y: ";
-        cin >> coordenadaY;
-    }
-
-    int movimentacao(void)
-    {
-        
+        cout << "X = " << coordenadaX << ", Y = " << coordenadaY << endl;
     }
 };
 
-int main(void)
+int main()
 {
     Boneco boneco;
-    
-    boneco.getCoordenadas();
-
+    cout << "Posicao inicial: " << boneco.coordenadaX << ", " << boneco.coordenadaY << endl;
+    cout << "Digite a posicao X: ";
+    cin >> boneco.coordenadaX;
+    cout << "Digite a posicao Y: ";
+    cin >> boneco.coordenadaY;
+    boneco.movimentacao(boneco.coordenadaX, boneco.coordenadaY);
     return 0;
 }
