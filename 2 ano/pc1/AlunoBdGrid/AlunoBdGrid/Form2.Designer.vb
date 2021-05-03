30320972f2494f2dafe6fa4ac71797cc13ff5190
Thiago Pereira <pereira.tp.thiago@gmail.com>
2021-05-03 16:58:00 -0300

    pc1

---
+++ 2 ano/pc1/AlunoBdGrid/AlunoBdGrid/Form2.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtCodigo = New System.Windows.Forms.TextBox()
+        Me.txtTurma = New System.Windows.Forms.TextBox()
+        Me.txtSerie = New System.Windows.Forms.TextBox()
+        Me.txtNome = New System.Windows.Forms.TextBox()
+        Me.btnSalvar = New System.Windows.Forms.Button()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Código"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(35, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(31, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Série"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(35, 68)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Nome"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(35, 117)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(37, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Turma"
+        '
+        'txtCodigo
+        '
+        Me.txtCodigo.Location = New System.Drawing.Point(81, 38)
+        Me.txtCodigo.Name = "txtCodigo"
+        Me.txtCodigo.ReadOnly = True
+        Me.txtCodigo.Size = New System.Drawing.Size(100, 20)
+        Me.txtCodigo.TabIndex = 4
+        '
+        'txtTurma
+        '
+        Me.txtTurma.Location = New System.Drawing.Point(81, 117)
+        Me.txtTurma.Name = "txtTurma"
+        Me.txtTurma.Size = New System.Drawing.Size(100, 20)
+        Me.txtTurma.TabIndex = 5
+        '
+        'txtSerie
+        '
+        Me.txtSerie.Location = New System.Drawing.Point(81, 91)
+        Me.txtSerie.Name = "txtSerie"
+        Me.txtSerie.Size = New System.Drawing.Size(100, 20)
+        Me.txtSerie.TabIndex = 6
+        '
+        'txtNome
+        '
+        Me.txtNome.Location = New System.Drawing.Point(81, 65)
+        Me.txtNome.Name = "txtNome"
+        Me.txtNome.Size = New System.Drawing.Size(100, 20)
+        Me.txtNome.TabIndex = 7
+        '
+        'btnSalvar
+        '
+        Me.btnSalvar.Location = New System.Drawing.Point(40, 161)
+        Me.btnSalvar.Name = "btnSalvar"
+        Me.btnSalvar.Size = New System.Drawing.Size(67, 28)
+        Me.btnSalvar.TabIndex = 8
+        Me.btnSalvar.Text = "Salvar"
+        Me.btnSalvar.UseVisualStyleBackColor = True
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.Location = New System.Drawing.Point(114, 161)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(67, 28)
+        Me.btnCancelar.TabIndex = 9
+        Me.btnCancelar.Text = "Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = True
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(268, 245)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.btnSalvar)
+        Me.Controls.Add(Me.txtNome)
+        Me.Controls.Add(Me.txtSerie)
+        Me.Controls.Add(Me.txtTurma)
+        Me.Controls.Add(Me.txtCodigo)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtCodigo As System.Windows.Forms.TextBox
+    Friend WithEvents txtTurma As System.Windows.Forms.TextBox
+    Friend WithEvents txtSerie As System.Windows.Forms.TextBox
+    Friend WithEvents txtNome As System.Windows.Forms.TextBox
+    Friend WithEvents btnSalvar As System.Windows.Forms.Button
+    Friend WithEvents btnCancelar As System.Windows.Forms.Button
+End Class
---
+++ 2 ano/pc1/AlunoBdGrid/AlunoBdGrid/Form2.vb
@@ -0,0 +1,47 @@
+﻿Public Class Form2
+    Dim mAluno As New Aluno
+    Dim mnovo As Boolean
+    Dim bd As New BdAluno
+    Private Sub alunototela()
+        txtcodigo.Text = mAluno.codigo
+        txtnome.Text = mAluno.nome
+        txtserie.Text = mAluno.serie
+        txtturma.Text = mAluno.turma
+    End Sub
+    Private Sub telatoaluno()
+        mAluno.codigo = txtcodigo.Text
+        mAluno.serie = txtserie.Text
+        mAluno.nome = txtnome.Text
+        mAluno.turma = txtturma.Text
+    End Sub
+    Public Property aluno()
+        Get
+            Return mAluno
+        End Get
+        Set(ByVal value)
+            mAluno = value
+            alunototela()
+        End Set
+    End Property
+    Public Property novo()
+        Get
+            Return mnovo
+        End Get
+        Set(ByVal value)
+            mnovo = value
+        End Set
+    End Property
+    Private Sub btnCancelar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCancelar.Click
+        Me.Dispose()
+    End Sub
+
+    Private Sub btnSalvar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSalvar.Click
+        telatoaluno()
+        If novo Then
+            bd.inserir(aluno)
+        Else
+            bd.atualizar(aluno)
+        End If
+        Me.Dispose()
+    End Sub
+End Class
