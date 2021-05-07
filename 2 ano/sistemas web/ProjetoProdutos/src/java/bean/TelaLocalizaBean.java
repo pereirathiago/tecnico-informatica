6ef7887c2f6228ab808da229b1328bb989e84077
Thiago Pereira <pereira.tp.thiago@gmail.com>
2021-05-07 16:52:53 -0300

    Update TelaLocalizaBean.java

--- 2 ano/sistemas web/ProjetoProdutos/src/java/bean/TelaLocalizaBean.java
+++ 2 ano/sistemas web/ProjetoProdutos/src/java/bean/TelaLocalizaBean.java
@@ -5,10 +5,74 @@
  */
 package bean;
 
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import persistencia.ProdutoDAO;
+import vo.Produto;
+
 /**
  *
  * @author 2info2021
  */
+@ManagedBean
+@RequestScoped
 public class TelaLocalizaBean {
+    private DataModel<Produto> lista;
+    ProdutoDAO pd=new ProdutoDAO();
+    private Produto produto = new Produto();
+    public TelaLocalizaBean() {
+        
+    }
+    public String atualizaLista() {
+        lista=new ListDataModel(pd.pesquisa());
+        return "index";
+    }
+
+    public DataModel<Produto> getLista() {
+        atualizaLista();
+        return lista;
+    }
+    public Produto produtoSelecionado() {
+        Produto p=lista.getRowData();
+        return p;
+    }
+    public String excluir(){
+        Produto p=produtoSelecionado();
+        pd.exclui(p);
+        return "index";
+    }
+    public String novo() {
+        setProduto(new Produto());
+        return "produto";
+    }
+    public String editar() {
+        Produto p=produtoSelecionado();
+        setProduto(p);
+        return "produto";
+    }
+    public String salva(){
+        pd.salva(getProduto());
+        return "index";
+    }
+    public String cancela() {
+        return "index";
+    }
+
+    /**
+     * @return the produto
+     */
+    public Produto getProduto() {
+        return produto;
+    }
+
+    /**
+     * @param produto the produto to set
+     */
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
     
 }
