56efa5adabb4a9446d8f3e3714af4ff6a70b60c2
Thiago Pereira <pereira.tp.thiago@gmail.com>
2021-05-10 13:25:44 -0300

    Create Bd.vb

---
+++ 2 ano/pc1/ProjetoEquipamentos/ProjetoEquipamentos/Bd.vb
@@ -0,0 +1,9 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Bd
+    Public Property User As String
+    Public Property bd As String
+    Public Property Senha As String
+    Public Property Servidor As String
+    Public Property Conexao As MySqlConnection
+
+End Class
