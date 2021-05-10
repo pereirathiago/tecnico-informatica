0bf18bdeb52ad19350f9651e9f4a4727cb61eb79
Thiago Pereira <pereira.tp.thiago@gmail.com>
2021-05-10 13:25:40 -0300

    Create Equipamento.vb

---
+++ 2 ano/pc1/ProjetoEquipamentos/ProjetoEquipamentos/Equipamento.vb
@@ -0,0 +1,7 @@
+﻿Public Class Equipamento
+    Public Property descricao As String
+    Public Property fabricante As String
+    Public Property numserie As String
+    Public Property numpatrimonio As Integer
+    Public Property localizacao As String
+End Class
