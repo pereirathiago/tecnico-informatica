1049def9dad84d61d91486cdfdd19d03f9b91959
Thiago Pereira <pereira.tp.thiago@gmail.com>
2021-05-10 13:30:32 -0300

    Update Bd.vb

--- 2 ano/pc1/ProjetoEquipamentos/ProjetoEquipamentos/Bd.vb
+++ 2 ano/pc1/ProjetoEquipamentos/ProjetoEquipamentos/Bd.vb
@@ -5,5 +5,18 @@ Public Class Bd
     Public Property Senha As String
     Public Property Servidor As String
     Public Property Conexao As MySqlConnection
-
+    Public Sub New()
+        Conexao = New MySqlConnection
+    End Sub
+    Public Sub Abrir()
+        Dim strConnString As String = "Data Source=" & Servidor & ";user id=" & User & ";password=" & "; database=" & bd & ";sslmode=none;"
+        If Conexao Is Nothing Or Conexao.State = ConnectionState.Broken Or Conexao.State = ConnectionState.Closed Then
+            Conexao = New MySqlConnection()
+            Conexao.ConnectionString = strConnString
+            Conexao.Open()
+        End If
+    End Sub
+    Public Sub fechar()
+        Conexao.Close()
+    End Sub
 End Class
