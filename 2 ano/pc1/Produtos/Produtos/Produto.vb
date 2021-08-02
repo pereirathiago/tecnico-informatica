10a1378599a009433a40c62f2ac669e755d7d294
Thiago Pereira <pereira.tp.thiago@gmail.com>
2021-08-02 13:26:28 -0300

    Create Produto.vb

---
+++ 2 ano/pc1/Produtos/Produtos/Produto.vb
@@ -0,0 +1,7 @@
+﻿Public Class Produto
+    Public Property id As Integer
+    Public Property codbar As String
+    Public Property descricao As String
+    Public Property quantidade As Double
+    Public Property minimo As Double
+End Class
