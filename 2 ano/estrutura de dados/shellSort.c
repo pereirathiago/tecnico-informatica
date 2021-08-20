11149a072aa50b19af27264cd67ec0d3cf5ce2d2
Thiago Pereira <pereira.tp.thiago@gmail.com>
2021-08-20 16:34:03 -0300

    estrutura de dados

---
+++ 2 ano/estrutura de dados/shellSort.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int v[10];
+
+void shellSort(int *v, int n)
+{
+    int i, j, value;
+
+    int h = 1;
+    while (h < n)
+    {
+        h = 3 * h + 1;
+    }
+    while (h > 0)
+    {
+        for (i = h; i < n; i++)
+        {
+            value = v[i];
+            j = i;
+            while (j > h - 1 && value <= v[j - h])
+            {
+                v[j] = v[j - h];
+                j = j - h;
+            }
+            v[j] = value;
+        }
+        h = h / 3;
+    }
+}
+
+int main()
+{
+    int i;
+    for (i = 0; i < 10; i++)
+    {
+        printf("Digite o valor");
+        scanf("%d", &v[i]);
+    }
+    shellSort(v, 10);
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d\n", v[i]);
+    }
+}
