f1f299132ebdd841ec0d3e3145c55287d2088ebb
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2021-08-31 09:56:39 -0300

    sistemas web - sistema de pedido de produto - tem um erro ai

--- 2 ano/sistemas web/MiniSistemaPedidoProduto/src/java/bean/TelaLocalizaBean.java
+++ 2 ano/sistemas web/MiniSistemaPedidoProduto/src/java/bean/TelaLocalizaBean.java
@@ -11,8 +11,10 @@ import javax.faces.bean.RequestScoped;
 import javax.faces.model.DataModel;
 import javax.faces.model.ListDataModel;
 import persistencia.ClienteDAO;
+import persistencia.PedidoDAO;
 import persistencia.ProdutoDAO;
 import vo.Cliente;
+import vo.Pedido;
 import vo.Produto;
 
 /**
@@ -22,98 +24,149 @@ import vo.Produto;
 @ManagedBean
 @RequestScoped
 public class TelaLocalizaBean implements Serializable {
+
     private DataModel<Produto> listaProduto;
     private ProdutoDAO pd = new ProdutoDAO();
     private Produto produto = new Produto();
-    
+
     private DataModel<Cliente> listaCliente;
     private ClienteDAO cd = new ClienteDAO();
     private Cliente cliente = new Cliente();
-    
-    public TelaLocalizaBean() {}
-    
+
+    private DataModel<Pedido> listaPedido;
+    private PedidoDAO ped = new PedidoDAO();
+    private Pedido pedido = new Pedido();
+
+    public TelaLocalizaBean() {
+    }
+
     public String atualizaListaProduto() {
         listaProduto = new ListDataModel(pd.pesquisa());
         return "produto";
     }
-    
+
     public DataModel<Produto> getListaProduto() {
         atualizaListaProduto();
         return listaProduto;
     }
-    
+
     public Produto produtoSelecionado() {
         Produto p = listaProduto.getRowData();
         return p;
     }
-    
+
     public String novoProduto() {
         produto = new Produto();
         return "cadastro_produto";
     }
-    
+
     public String excluirProduto() {
         Produto p = produtoSelecionado();
         pd.exclui(p);
         return "produto";
     }
-    
+
     public String editarProduto() {
         Produto p = produtoSelecionado();
         setProduto(p);
         return "cadastro_produto";
     }
-    
+
     public String salvaProduto() {
         pd.salva(getProduto());
         return "produto";
     }
-    
+
     public String cancelaProduto() {
         return "produto";
     }
-    
+
     public String atualizaListaCliente() {
         listaCliente = new ListDataModel(cd.pesquisa());
         return "cliente";
     }
-    
+
     public DataModel<Cliente> getListaCliente() {
         atualizaListaCliente();
         return listaCliente;
     }
-    
+
     public Cliente clienteSelecionado() {
         Cliente c = listaCliente.getRowData();
         return c;
     }
-    
+
     public String novoCliente() {
         setCliente(new Cliente());
         return "cadastro_cliente";
     }
-    
+
     public String excluirCliente() {
         Cliente c = clienteSelecionado();
         cd.exclui(c);
         return "cliente";
     }
-    
+
     public String editarCliente() {
         Cliente c = clienteSelecionado();
         setCliente(c);
         return "cadastro_cliente";
     }
-    
+
     public String salvaCliente() {
         cd.salva(getCliente());
         return "cliente";
     }
-    
+
     public String cancelaCliente() {
         return "cliente";
     }
 
+    public String atualizaListaPedido() {
+        listaPedido = new ListDataModel(ped.pesquisa());
+        return "pedido";
+    }
+
+    public DataModel<Pedido> getListaPedido() {
+        atualizaListaPedido();
+        return listaPedido;
+    }
+
+    public Pedido pedidoSelecionado() {
+        Pedido p = getListaPedido().getRowData();
+        return p;
+    }
+
+    public String excluirPedido() {
+        Pedido p = pedidoSelecionado();
+        ped.exclui(p);
+        return "pedido";
+    }
+
+    public String novoPedido() {
+        setPedido(new Pedido());
+        return "cadastro_pedido";
+    }
+
+    public String editarPedido() {
+        Pedido p = pedidoSelecionado();
+        setPedido(p);
+        return "cadastro_pedido";
+    }
+    
+    public String salvaPedido() {
+        if(pd.localiza(getPedido().getIdProduto()) != null) {
+            if(cd.localiza(getPedido().getIdCliente()) != null) {
+                ped.salva(getPedido());
+                return "pedido";
+            }
+        }
+        return "";
+    }
+    
+    public String cancelaPedido() {
+        return "pedido";
+    }
     /**
      * @return the produto
      */
@@ -140,5 +193,19 @@ public class TelaLocalizaBean implements Serializable {
      */
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
+    }
+
+    /**
+     * @return the pedido
+     */
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    /**
+     * @param pedido the pedido to set
+     */
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
     }
 }
---
+++ 2 ano/sistemas web/MiniSistemaPedidoProduto/src/java/persistencia/PedidoDAO.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistencia;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import vo.Pedido;
+
+/**
+ *
+ * @author Thiago
+ */
+public class PedidoDAO {
+    EntityManager em;
+    public PedidoDAO() {
+        em = EntityManagerProvider.getEM();
+    }
+    
+    public void salva(Pedido p) {
+        em.getTransaction().begin();
+        if (p.getIdPedido() == 0)
+            em.persist(p);
+        else 
+            em.merge(p);
+        em.getTransaction().commit();
+    }
+    
+    public void exclui(Pedido p) {
+        em.getTransaction().begin();
+        em.remove(p);
+        em.getTransaction().commit();
+    }
+    
+    public List<Pedido> pesquisa() {
+        Query q = em.createQuery("select p from Pedido p");
+        List<Pedido> listaPedido = q.getResultList();
+        return listaPedido;
+    }
+}
