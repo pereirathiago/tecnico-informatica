49c463925bc030694ff2e8b99746e9c5261efeb9
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2021-08-31 08:44:23 -0300

    sistemas web - pedidoProdutos - pedido vo

---
+++ 2 ano/sistemas web/MiniSistemaPedidoProduto/src/java/vo/Pedido.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vo;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Thiago
+ */
+@Entity
+public class Pedido implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idPedido = 0;
+    private Integer idCliente;
+    private Integer idProduto;
+    private Integer qtdPedido;
+    public Pedido(Integer idPedido) {
+        this.idPedido = idPedido;
+    }
+    public Pedido(){}
+
+    /**
+     * @return the idPedido
+     */
+    public Integer getIdPedido() {
+        return idPedido;
+    }
+
+    /**
+     * @param idPedido the idPedido to set
+     */
+    public void setIdPedido(Integer idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    /**
+     * @return the idCliente
+     */
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+
+    /**
+     * @param idCliente the idCliente to set
+     */
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    /**
+     * @return the idProduto
+     */
+    public Integer getIdProduto() {
+        return idProduto;
+    }
+
+    /**
+     * @param idProduto the idProduto to set
+     */
+    public void setIdProduto(Integer idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    /**
+     * @return the qtdPedido
+     */
+    public Integer getQtdPedido() {
+        return qtdPedido;
+    }
+
+    /**
+     * @param qtdPedido the qtdPedido to set
+     */
+    public void setQtdPedido(Integer qtdPedido) {
+        this.qtdPedido = qtdPedido;
+    }
+}
