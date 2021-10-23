062e43073f0641e7d3bae0e0a657efc3c96c3e14
ThiagoPeriera232 <pereira.tp.thiago@gmail.com>
2021-10-23 10:43:17 -0300

    estrutura de dados - paciencia

--- 2 ano/estrutura de dados/Paciencia/Assets/Scripts/Paciencia.cs
+++ 2 ano/estrutura de dados/Paciencia/Assets/Scripts/Paciencia.cs
@@ -1,20 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Paciencia : MonoBehaviour
 {
     public Sprite[] faceCartas;
     public GameObject cardPrefab;
+    public GameObject[] bottomPos;
+    public GameObject[] topPos;
 
     public static string[] naipes = new string[] { "P", "O", "C", "E" };
     public static string[] valores = new string[] { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+    public List<string>[] bottoms;
+    public List<string>[] tops;
+
+    private List<string> bottom0 = new List<string>();
+    private List<string> bottom1 = new List<string>();
+    private List<string> bottom2 = new List<string>();
+    private List<string> bottom3 = new List<string>();
+    private List<string> bottom4 = new List<string>();
+    private List<string> bottom5 = new List<string>();
+    private List<string> bottom6 = new List<string>();
 
     public List<string> baralho;
 
     // Start is called before the first frame update
     void Start()
     {
+        bottoms = new List<string>[] { bottom0, bottom1, bottom2, bottom3, bottom4, bottom5, bottom6 };
         JogarCartas();
     }
 
@@ -32,6 +46,7 @@ public class Paciencia : MonoBehaviour
         {
             print(carta);
         }
+        PacienciaSort();
         PacienciaDeal();
     }
 
@@ -64,16 +79,31 @@ public class Paciencia : MonoBehaviour
 
     void PacienciaDeal()
     {
-        float yOffset = 0;
-        float zOffset = 0.03f;
-        foreach(string carta in baralho)
+        for (int i = 0; i < 7; i++)
         {
-            GameObject novaCarta = Instantiate(cardPrefab, new Vector3(transform.position.x, transform.position.y - yOffset, transform.position.z - zOffset), Quaternion.identity);
-            novaCarta.name = carta;
-            novaCarta.GetComponent<Selecionado>().faceCima = true;
+            float yOffset = 0;
+            float zOffset = 0.03f;
+            foreach (string carta in bottoms[i])
+            {
+                GameObject novaCarta = Instantiate(cardPrefab, new Vector3(bottomPos[i].transform.position.x, bottomPos[i].transform.position.y - yOffset, bottomPos[i].transform.position.z - zOffset), Quaternion.identity, bottomPos[i].transform);
+                novaCarta.name = carta;
+                novaCarta.GetComponent<Selecionado>().faceCima = true;
 
-            yOffset = yOffset + 0.5f;
-            zOffset = zOffset + 0.03f;
+                yOffset = yOffset + 0.5f;
+                zOffset = zOffset + 0.03f;
+            }
+        }
+    }
+
+    void PacienciaSort()
+    {
+        for(int i = 0; i < 7; i++)
+        {
+            for(int j = i; j < 7; j++)
+            {
+                bottoms[j].Add(baralho.Last<string>());
+                baralho.RemoveAt(baralho.Count - 1);
+            }
         }
     }
 }
