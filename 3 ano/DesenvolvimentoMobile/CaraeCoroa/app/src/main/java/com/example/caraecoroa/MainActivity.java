82e51257ea1f39d3664043cf9af62e72fa267eed
Thiago Pereira <pereira.tp.thiago@gmail.com>
2022-03-08 16:32:44 -0300

    caraECoroa

--- 3 ano/DesenvolvimentoMobile/CaraeCoroa/app/src/main/java/com/example/caraecoroa/MainActivity.java
+++ 3 ano/DesenvolvimentoMobile/CaraeCoroa/app/src/main/java/com/example/caraecoroa/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.caraecoroa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +18,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bSorteia = findViewById(R.id.bSorteia);
+        iMoeda = findViewById(R.id.iMoeda);
+
+        bSorteia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int sorteia = new Random().nextInt(2);
+                if(sorteia == 1){
+                    iMoeda.setImageResource(R.drawable.cara);
+                } else{
+                    iMoeda.setImageResource(R.drawable.coroa);
+                }
+            }
+        });
     }
 }
