583c24ac6660d10d1421772480f20cbdab8e6b56
Thiago Pereira <pereira.tp.thiago@gmail.com>
2022-03-15 17:15:14 -0300

    andorid calendario

--- 3 ano/DesenvolvimentoMobile/IdadelCalendario/app/src/main/java/com/example/idadelcalendario/MainActivity.java
+++ 3 ano/DesenvolvimentoMobile/IdadelCalendario/app/src/main/java/com/example/idadelcalendario/MainActivity.java
@@ -8,11 +8,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText tIdade;
+    EditText tData;
     TextView tResultado;
     Button bCalcula;
 
@@ -21,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tIdade =findViewById(R.id.tIdade);
+        tData =findViewById(R.id.tDatanasc);
         tResultado = findViewById(R.id.tResultado);
         bCalcula = findViewById(R.id.bCalcular);
 
@@ -30,6 +32,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Calendario c = new Calendario();
                 SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                try {
+                    c.setDatanasc(Calendar.getInstance());
+                    c.getDatanasc().setTime(sdf.parse(tData.getText().toString()));
+                    c.calcula();
+                    tResultado.setText("Você viveu " + c.getMes()+" meses, ou " + c.getDia() + " dias, ou " + c.getHora() + " horas, ou " + c.getMinuto() + " minutos.");
+                } catch (ParseException e) {
+                    tResultado.setText("Erro de data");
+                }
             }
         });
     }
