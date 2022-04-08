84628118e7a1d3cf689c029c40573e7d99ab0751
Thiago Pereira <pereira.tp.thiago@gmail.com>
2022-04-08 19:14:52 -0300

    folha pagamento bd

---
+++ 3 ano/DesenvolvimentoDeSistemas/SistemaSalario/SistemaSalario/bd/BdFuncionario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaSalario.bd
+{
+    internal class BdFuncionario : Bd
+    {
+        public BdFuncionario()
+        {
+            User = "root";
+            Servidor = "localhost";
+            Senha = "vertrigo";
+            bd = "folhaPagamento";
+        }
+    }
+}
