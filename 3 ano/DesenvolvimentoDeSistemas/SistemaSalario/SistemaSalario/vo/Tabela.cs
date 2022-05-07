8654488e16a363d9c552ba9ac0289fe534f359dd
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-05-07 18:20:26 -0300

    Create Tabela.cs

---
+++ 3 ano/DesenvolvimentoDeSistemas/SistemaSalario/SistemaSalario/vo/Tabela.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaSalario.vo
+{
+    internal class Tabela
+    {
+        private double tinss1;
+        private double tinss2;
+        private double tinss3;
+        private double tinss4;
+        private double ainss1;
+        private double ainss2;
+        private double ainss3;
+        private double ainss4;
+        private double tsf;
+        private double vsf;
+        private double tirrf1;
+        private double tirrf2;
+        private double airrf2;
+        private double dirrf2;
+        private double tirrf3;
+        private double airrf3;
+        private double dirrf3;
+        private double tirrf4;
+        private double airrf4;
+        private double dirrf4;
+        private double airrf5;
+        private double dirrf5;
+        private double dedpdep;
+
+        public double Tinss1 { get => tinss1; set => tinss1 = value; }
+        public double Tinss2 { get => tinss2; set => tinss2 = value; }
+        public double Tinss3 { get => tinss3; set => tinss3 = value; }
+        public double Tinss4 { get => tinss4; set => tinss4 = value; }
+        public double Ainss1 { get => ainss1; set => ainss1 = value; }
+        public double Ainss2 { get => ainss2; set => ainss2 = value; }
+        public double Ainss3 { get => ainss3; set => ainss3 = value; }
+        public double Ainss4 { get => ainss4; set => ainss4 = value; }
+        public double Tsf { get => tsf; set => tsf = value; }
+        public double Vsf { get => vsf; set => vsf = value; }
+        public double Tirrf1 { get => tirrf1; set => tirrf1 = value; }
+        public double Tirrf2 { get => tirrf2; set => tirrf2 = value; }
+        public double Airrf2 { get => airrf2; set => airrf2 = value; }
+        public double Dirrf2 { get => dirrf2; set => dirrf2 = value; }
+        public double Tirrf3 { get => tirrf3; set => tirrf3 = value; }
+        public double Airrf3 { get => airrf3; set => airrf3 = value; }
+        public double Dirrf3 { get => dirrf3; set => dirrf3 = value; }
+        public double Tirrf4 { get => tirrf4; set => tirrf4 = value; }
+        public double Airrf4 { get => airrf4; set => airrf4 = value; }
+        public double Dirrf4 { get => dirrf4; set => dirrf4 = value; }
+        public double Airrf5 { get => airrf5; set => airrf5 = value; }
+        public double Dirrf5 { get => dirrf5; set => dirrf5 = value; }
+        public double Dedpdep { get => dedpdep; set => dedpdep = value; }
+    }
+}
