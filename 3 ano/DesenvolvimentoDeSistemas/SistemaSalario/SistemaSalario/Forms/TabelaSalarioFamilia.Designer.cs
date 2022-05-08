bb5c276a39bde88cc637d42c0f039c0f39780a24
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-05-08 18:39:10 -0300

    tela salario familia

---
+++ 3 ano/DesenvolvimentoDeSistemas/SistemaSalario/SistemaSalario/Forms/TabelaSalarioFamilia.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace SistemaSalario.Forms
+{
+    partial class TabelaSalarioFamilia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.funcionarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabelasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iNSSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salárioFamíliaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iRRFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnAtualizarInss = new System.Windows.Forms.Button();
+            this.txtTsf = new System.Windows.Forms.MaskedTextBox();
+            this.txtVsf = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(413, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.funcionarioToolStripMenuItem,
+            this.tabelasToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // funcionarioToolStripMenuItem
+            // 
+            this.funcionarioToolStripMenuItem.Name = "funcionarioToolStripMenuItem";
+            this.funcionarioToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.funcionarioToolStripMenuItem.Text = "Funcionario";
+            // 
+            // tabelasToolStripMenuItem
+            // 
+            this.tabelasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iNSSToolStripMenuItem,
+            this.salárioFamíliaToolStripMenuItem,
+            this.iRRFToolStripMenuItem});
+            this.tabelasToolStripMenuItem.Name = "tabelasToolStripMenuItem";
+            this.tabelasToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.tabelasToolStripMenuItem.Text = "Tabelas";
+            // 
+            // iNSSToolStripMenuItem
+            // 
+            this.iNSSToolStripMenuItem.Name = "iNSSToolStripMenuItem";
+            this.iNSSToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.iNSSToolStripMenuItem.Text = "INSS";
+            // 
+            // salárioFamíliaToolStripMenuItem
+            // 
+            this.salárioFamíliaToolStripMenuItem.Name = "salárioFamíliaToolStripMenuItem";
+            this.salárioFamíliaToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.salárioFamíliaToolStripMenuItem.Text = "Salário Família";
+            // 
+            // iRRFToolStripMenuItem
+            // 
+            this.iRRFToolStripMenuItem.Name = "iRRFToolStripMenuItem";
+            this.iRRFToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.iRRFToolStripMenuItem.Text = "IRRF";
+            // 
+            // btnAtualizarInss
+            // 
+            this.btnAtualizarInss.Location = new System.Drawing.Point(144, 206);
+            this.btnAtualizarInss.Name = "btnAtualizarInss";
+            this.btnAtualizarInss.Size = new System.Drawing.Size(124, 37);
+            this.btnAtualizarInss.TabIndex = 45;
+            this.btnAtualizarInss.Text = "Atualizar Salário Família";
+            this.btnAtualizarInss.UseVisualStyleBackColor = true;
+            // 
+            // txtTsf
+            // 
+            this.txtTsf.Location = new System.Drawing.Point(70, 159);
+            this.txtTsf.Name = "txtTsf";
+            this.txtTsf.Size = new System.Drawing.Size(111, 20);
+            this.txtTsf.TabIndex = 38;
+            // 
+            // txtVsf
+            // 
+            this.txtVsf.Location = new System.Drawing.Point(231, 159);
+            this.txtVsf.Name = "txtVsf";
+            this.txtVsf.Size = new System.Drawing.Size(111, 20);
+            this.txtVsf.TabIndex = 34;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(206, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 18);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "Valor por dependente";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(97, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 18);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "Salário";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Até";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(133, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 22);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "Salário Família";
+            // 
+            // TabelaSalarioFamilia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 303);
+            this.Controls.Add(this.btnAtualizarInss);
+            this.Controls.Add(this.txtTsf);
+            this.Controls.Add(this.txtVsf);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "TabelaSalarioFamilia";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SalarioFamilia";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem funcionarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tabelasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iNSSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salárioFamíliaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iRRFToolStripMenuItem;
+        private System.Windows.Forms.Button btnAtualizarInss;
+        private System.Windows.Forms.MaskedTextBox txtTsf;
+        private System.Windows.Forms.TextBox txtVsf;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
