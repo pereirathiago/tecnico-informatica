3e11bafb85c71fcd95466ee0aac21d03894c671d
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-05-16 21:36:50 -0300

    routes

--- 3 ano/DesenvolvimentoDeSistemas/SistemaMensagem/api/src/server.js
+++ 3 ano/DesenvolvimentoDeSistemas/SistemaMensagem/api/src/server.js
@@ -1,10 +1,10 @@
 import express from 'express';
+
+import {router} from './routes'
+
 const app = express();
 
 app.use(express.json())
-
-app.get("/", (req, res) => {
-    return res.json({hello: 'world'});
-})
+app.use(router)
 
 app.listen(3030, () => console.log('Server listening on port 3030'))
