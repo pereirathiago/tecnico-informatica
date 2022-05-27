84024318c3505fe9a0983cd7656ccf791d023c69
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-05-27 16:43:15 -0300

    nav bar

--- 3 ano/DesenvolvimentoDeSistemas/SistemaMensagem/mensagem_email/components/Navbar.js
+++ 3 ano/DesenvolvimentoDeSistemas/SistemaMensagem/mensagem_email/components/Navbar.js
@@ -1,27 +1,39 @@
+import { useState, useEffect } from "react";
+import { IoExitOutline } from 'react-icons/io5';
+
+import cookie from 'js-cookie'
 import Image from "next/image"
 import Link from "next/link";
 
 import styles from '../styles/Navbar.module.css'
 
 export default function Navbar(){
+
+    const [cookieAuth, setCookieAuth] = useState(false)
+
+    useEffect(() => {
+        setCookieAuth(cookie.get('user-auth') || false)
+    }, [])
+
     return (
         <nav className={styles.navbar}>
-            <div className={styles.logo}>
-                <Image src="/mail.svg" width="30" height="30" alt="Mail"/>
-                <h1>Gmail²</h1>
-            </div>
+            {cookieAuth && 
+                <div className={styles.logo}>
+                    <Image src="/mail.svg" width="30" height="30" alt="Mail"/>
+                    <h1>Gmail²</h1>
+                </div>
+            }
             <ul className={styles.link_items}>
-            {{
-                __html: `if(!document.cookie || !document.cookie.includes('controle-estagio-auth')){
-                    window.location.href = "/login"
-                }`,
-            }}
-                <li>
-                    <Link href="/"><a>Home</a></Link>
-                </li>
-                <li>
-                    <Link href="/about"><a>Sobre</a></Link>
-                </li>
+                {cookieAuth &&
+                    <li>
+                        <Link href="/">
+                            <a>
+                                <IoExitOutline />
+                                Sair
+                            </a>
+                        </Link>
+                    </li>
+                }
             </ul>
         </nav>
     )
