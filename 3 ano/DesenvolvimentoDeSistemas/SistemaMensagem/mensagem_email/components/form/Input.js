eb0c9b29015983165e734c80a0d278349bf4ed0f
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-05-27 20:19:59 -0300

    input

--- 3 ano/DesenvolvimentoDeSistemas/SistemaMensagem/mensagem_email/components/form/Input.js
+++ 3 ano/DesenvolvimentoDeSistemas/SistemaMensagem/mensagem_email/components/form/Input.js
@@ -4,7 +4,7 @@ export default function Input({type, text, name, placeholder, handleOnChange, va
     return (
         <div className={styles.form_control}>
             <label forhtml={name}>{text}:</label>
-            <input type={type} name={name} id={name} placeholder={placeholder} onChange={handleOnChange} value={value}/>
+            <input type={type} name={name} id={name} placeholder={placeholder} onChange={handleOnChange} value={value} required/>
         </div>
     )
 }
--- 3 ano/DesenvolvimentoDeSistemas/SistemaMensagem/mensagem_email/pages/login.js
+++ 3 ano/DesenvolvimentoDeSistemas/SistemaMensagem/mensagem_email/pages/login.js
@@ -3,11 +3,38 @@ import Image from 'next/image'
 import Input from '../components/form/Input'
 import SubmitButton from '../components/form/SubmitButton'
 
+import { useState } from 'react'
+
 import styles from '../styles/Login.module.css'
 
 export default function Login() {
-    const login = async (e) => {
+
+    const [user, setUser] = useState('')
+    const [password, setPassword] = useState('')
+
+    const submit = (e) => {
         e.preventDefault()
+        if(user == 'admin' && password == '1234') {
+            window.location.href = '/admin'
+        } else {
+            verifyLogin()
+        }
+    }
+
+    const verifyLogin = async () => {
+        const response = await fetch(`${process.env.API}/user/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({user, password})
+        })
+        const data = await response.json()
+        if(data.status == 200) {
+            alert('conectado')
+        } else {
+            alert('Usuário ou senha incorretos')
+        }
     }
 
     return (
@@ -25,10 +52,10 @@ export default function Login() {
                     <Image src="/mailbox_re_dvds.svg" alt="Gmail²" width={300} height={300} />
                 </div>
                 <div className={styles.form}>
-                    <form onSubmit={login}>
+                    <form onSubmit={submit}>
                         <h1>Login</h1>
-                        <Input type="text" text="Seu email" name="email" placeholder="Digite seu email"/>
-                        <Input type="password" text="Sua senha" name="password" placeholder="Digite sua senha"/>
+                        <Input type="text" text="Seu Usuário" name="user" placeholder="Digite seu usuário" handleOnChange={(e) => e.target.value} value={user}/>
+                        <Input type="password" text="Sua senha" name="password" placeholder="Digite sua senha" handleOnChange={(e) => e.target.value} value={password}/>
                         <SubmitButton text={'Entrar'}/>
                     </form>
                 </div>
