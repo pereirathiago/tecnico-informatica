34751e9bb4914588870183e1335a46ae9a3e8f7b
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-05-27 21:44:39 -0300

    Update login.js

--- 3 ano/DesenvolvimentoDeSistemas/SistemaMensagem/mensagem_email/pages/login.js
+++ 3 ano/DesenvolvimentoDeSistemas/SistemaMensagem/mensagem_email/pages/login.js
@@ -25,7 +25,7 @@ export default function Login() {
     }
 
     const verifyLogin = async () => {
-        fetch("http://localhost:3030/user/login", {
+        fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json; charset=utf-8'
