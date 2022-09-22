c57c893f922683b759926d6f7b27098f9c4fec2f
Thiago Pereira <pereira.tp.thiago@gmail.com>
2022-09-22 17:08:45 -0300

    bds

---
+++ 3 ano/DesenvolvimentoDeSistemas/SistemaEmprestimo/SistemaEmprestimo/bd/BdEmprestimos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaEmprestimo.bd
+{
+    internal class BdEmprestimos : Bd
+    {
+        public BdEmprestimos()
+        {
+            User = "root";
+            Servidor = "localhost";
+            Senha = "vertrigo";
+            bd = "emprestimos";
+        }
+    }
+}
