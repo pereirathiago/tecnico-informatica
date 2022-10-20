a9c593cc51b108798ec8090b34c93e9646d6cd9f
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-10-20 11:15:12 -0300

    api admin user

--- 3 ano/DesenvolvimentoDeSistemas/SistemaRestaurante/src/controllers/UserController.js
+++ 3 ano/DesenvolvimentoDeSistemas/SistemaRestaurante/src/controllers/UserController.js
@@ -30,4 +30,101 @@ export default {
         }
     },
 
+    async findAllUsers(req, res) {
+        try {
+            const users = await prisma.admin_users.findMany()
+            return res.status(200).json(users)
+        } catch (error) {
+            return res.json({ error })
+        } finally {
+            ;async () => {
+                await prisma.$disconnect()
+            }
+        }
+    }, 
+
+    async findUser(req, res) {
+        try {
+            const { username } = req.params
+            const user = await prisma.admin_users.findUnique({ where: { username } })
+            if (!user)
+                return res.status(404).json({ message: 'Usurario não encontrado' })
+            return res.status(200).json(user)
+        } catch (error) {
+            return res.json({ error })
+        } finally {
+            ;async () => {
+                await prisma.$disconnect()
+            }
+        }
+    },
+
+    async updateUser(req, res) {
+        try {
+            const { username } = req.params
+            const { nome, senha } = req.body
+
+            let user = await prisma.admin_users.findUnique({ where: { username } })
+            if (!user)
+                return res.status(404).json({ message: 'Usurario não encontrado' })
+
+            user = await prisma.admin_users.update({
+                where: { username },
+                data: { nome, senha }
+            })
+
+            return res.status(200).json({
+                message: 'Usuário atualizado com sucesso',
+                user,
+            })
+        } catch (error) {
+            return res.json({ error })
+        } finally {
+            ;async () => {
+                await prisma.$disconnect()
+            }
+        }
+    },
+
+    async deleteUser(req, res) {
+        try {
+            const { username } = req.params
+            const user = await prisma.admin_users.findUnique({ where: { username } })
+            if (!user)
+                return res.status(404).json({ message: 'Usurario não encontrado' })
+
+            await prisma.admin_users.delete({ where: { username } })
+
+            return res.status(200).json({
+                message: 'Usuário deletado com sucesso',
+                user,
+            })
+        } catch (error) {
+            return res.json({ error })
+        } finally {
+            ;async () => {
+                await prisma.$disconnect()
+            }
+        }
+    },
+
+    async loginUser(req, res) {
+        try {
+            const { username, senha } = req.body
+            const user = await prisma.admin_users.findUnique({ where: { username: username }})
+    
+            if(user){
+                if(senha == user.senha){
+                    return res.status(200).json({ message: 'login feito' }) 
+                }
+            }
+            return res.status(401).json({message: "usuário ou senha incorreto"})
+        } catch (error) {
+            return res.json({ error })
+        } finally {
+            ;async () => {
+                await prisma.$disconnect()
+            }
+        }
+    }
 }
--- 3 ano/DesenvolvimentoDeSistemas/SistemaRestaurante/src/routes.js
+++ 3 ano/DesenvolvimentoDeSistemas/SistemaRestaurante/src/routes.js
@@ -4,6 +4,10 @@ import UserController from './controllers/UserController'
 const router = Router()
 
 router.post("/user", UserController.createUser)
-
+router.get("/users", UserController.findAllUsers)
+router.get("/user/:username", UserController.findUser)
+router.put("/user/:username", UserController.updateUser)
+router.delete("/user/:username", UserController.deleteUser)
+router.post("/user/login", UserController.loginUser)
 
 export { router }
