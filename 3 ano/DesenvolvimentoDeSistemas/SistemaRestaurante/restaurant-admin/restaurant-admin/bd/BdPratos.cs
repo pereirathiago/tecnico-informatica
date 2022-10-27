38818b20d361cb1d62ec535510c97c1b662e3b0a
Thiago Pereira <pereira.tp.thiago@gmail.com>
2022-10-27 14:53:26 -0300

    restaurante admi

---
+++ 3 ano/DesenvolvimentoDeSistemas/SistemaRestaurante/restaurant-admin/restaurant-admin/bd/BdPratos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace restaurant_admin.bd
+{
+    internal class BdPratos
+    {
+        public static void ListarPratos()
+        {
+            string urlApi = "http://localhost:"
+        }
+    }
+}
---
+++ 3 ano/DesenvolvimentoDeSistemas/SistemaRestaurante/restaurant-admin/restaurant-admin/vo/Funcionarios.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace restaurant_admin.vo
+{
+    internal class Funcionarios
+    {
+        private int _id;
+        private string _username;
+        private string _nome;
+        private string _senha;
+        private int _funcao;
+
+        public int Id { get => _id; set => _id = value; }
+        public string Username { get => _username; set => _username = value; }
+        public string Nome { get => _nome; set => _nome = value; }
+        public string Senha { get => _senha; set => _senha = value; }
+        public int Funcao { get => _funcao; set => _funcao = value; }
+    }
+}
