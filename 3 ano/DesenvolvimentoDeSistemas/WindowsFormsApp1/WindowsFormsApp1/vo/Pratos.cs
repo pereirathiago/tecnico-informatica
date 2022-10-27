d3e999841c51435f4ac38667515fc97010edce76
Thiago Pereira <pereira.tp.thiago@gmail.com>
2022-10-27 14:48:37 -0300

    criação adm c#

---
+++ 3 ano/DesenvolvimentoDeSistemas/WindowsFormsApp1/WindowsFormsApp1/vo/Pratos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.vo
+{
+    internal class Pratos
+    {
+        private int _id;
+        private string _nome;
+        private string _descricao;
+        private double _preco;
+
+        public int Id { get => _id; set => _id = value; }
+        public string Nome { get => _nome; set => _nome = value; }
+        public string Descricao { get => _descricao; set => _descricao = value; }
+        public double Preco { get => _preco; set => _preco = value; }
+    }
+}
