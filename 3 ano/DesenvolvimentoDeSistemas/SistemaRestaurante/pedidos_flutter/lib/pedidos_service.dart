a19e846c0c144a3a4cd843d65101b174634933cc
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-11-30 16:38:31 -0300

    Update pedidos_service.dart

--- 3 ano/DesenvolvimentoDeSistemas/SistemaRestaurante/pedidos_flutter/lib/pedidos_service.dart
+++ 3 ano/DesenvolvimentoDeSistemas/SistemaRestaurante/pedidos_flutter/lib/pedidos_service.dart
@@ -7,15 +7,13 @@ class PedidoService {
 
   static Future<int> login(Usuario usuario) async {
     try {
-      print('user: ${usuario.username} pass: ${usuario.senha}');
       final response = await http.post(
         Uri.parse('$url/user/login'),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
-        }, 
+        },
         body: jsonEncode(usuario),
       );
-      print('Response status: ${response.statusCode}');
       return response.statusCode == 200 ? 0 : 1;
     } catch (e) {
       throw Exception("Erro ao carregar inserir " + e.toString());
